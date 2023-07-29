53be83ea4cf3507d37b3ab220f907318f110d9ef
Akash kodali <akashkodali6@gmail.com>
2023-07-29 13:16:37 +0530

    lib-update

--- library_record.py
+++ library_record.py
@@ -1,1 +1,14 @@
+import numpy.random as ran
+
 TotalBooks = 100
+borrowedBooks = 20
+no_of_renewals = ran.randint(0, 5, borrowedBooks)
+print(no_of_renewals)
+
+print(f"{borrowedBooks} Books are borrowed out of {TotalBooks} \n")
+for i in range(borrowedBooks):
+    print(f"Book {i+1} has '{no_of_renewals[i]}' no of renewals")
+
+print(f"Total No of borrows are: {sum(no_of_renewals)}")
+
+#preceision
